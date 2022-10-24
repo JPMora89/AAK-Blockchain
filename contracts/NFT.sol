a63a44022a8d9267de542191de9ab11825fd5e84
tahlil <bsse0803@iit.du.ac.bd>
2022-10-24 15:43:52 +0600

    Update solidity version

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
