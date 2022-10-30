4fe23a57d6d554830106301f9e1e9d0676d5b712
tahlil <bsse0803@iit.du.ac.bd>
2022-10-30 13:18:05 +0600

    Exatract req body for create asset pending api

--- pages/api/blockchain/create_asset_pending.js
+++ pages/api/blockchain/create_asset_pending.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-      res.status(200).json({name: "John Doe" })
+    const {new_env, creator_user_id, creator_username, creator_name, creator_metamask_id, associated_project_name, associated_project_url, asset_name, asset_type, asset_description, asset_file, asset_terms, asset_image, hidden, private, multi_nft, asset_price
+        } = req.body;
+      res.status(200).json({msg: "Success" })
     }
     res.status(400).json({ msg: 'Bad request' });
 }
