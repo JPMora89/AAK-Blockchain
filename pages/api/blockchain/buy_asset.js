9f1137c2ef76900211fe0a0f02d489a6b838b246
tahlil <bsse0803@iit.du.ac.bd>
2022-10-30 13:31:33 +0600

    Fix condition bug and extract req body for buy asset api

--- pages/api/blockchain/buy_asset.js
+++ pages/api/blockchain/buy_asset.js
@@ -1,4 +1,27 @@
 export default function handler(req, res) {
+  const {new_env,
+    user_id,
+    buyer_username,
+    buyer_name,
+    buyer_metamask_id,
+    asset_id,
+    r,
+    s,
+    v 
+   } = req.body;
+   if (
+    user_id == null ||
+    buyer_username == null ||
+    buyer_name == null ||
+    buyer_metamask_id == null ||
+    asset_id == null ||
+    r == null ||
+    s == null ||
+    v == null 
+   ) {
+     res.status(400).json({ msg: 'Bad request' });  
+   }
+
     if (req.method === 'POST') {
       res.status(200).json({name: "John Doe" })
     }
