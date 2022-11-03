2976121299f6433d306f014743d5acfe5ebc02ab
tahlil <bsse0803@iit.du.ac.bd>
2022-11-03 20:53:42 +0600

    Handle quary parameter

--- pages/api/blockchain/get_market_asset.js
+++ pages/api/blockchain/get_market_asset.js
@@ -1,6 +1,12 @@
 export default function handler(req, res) {
     if (req.method === 'GET') {
-      res.status(200).json({name: "John Doe" })
+        const { asset_id } = req.query;
+        if(asset_id == null){
+            res.status(400).json({msg: 'Bad request'});
+        }
+        res.status(200).json({name: "John Doe"})
     }
-    res.status(400).json({ msg: 'Bad request' });
+    else{
+        res.status(400).json({msg: 'Bad request'});
+    }
 }
