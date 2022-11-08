17a0993950674555574919230559e6f57d0d952d
tahlil <bsse0803@iit.du.ac.bd>
2022-11-08 19:24:28 +0600

    Process query param for getting private asset

--- pages/api/blockchain/get_private_asset.js
+++ pages/api/blockchain/get_private_asset.js
@@ -1,6 +1,10 @@
 export default function handler(req, res) {
     if (req.method === 'GET') {
-      res.status(200).json({name: "John Doe" })
+      const { asset_id, metamask_id } = req.query;
+        if(asset_id === null || metamask_id === null){
+            res.status(400).json({msg: 'Bad request'});
+        }
+        res.status(200).json({name: "John Doe"})
     }
     else{
       res.status(400).json({ msg: 'Bad request' });
