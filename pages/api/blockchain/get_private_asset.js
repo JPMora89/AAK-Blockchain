a2f4d3867d1e89d8c77d7336280a6334d72ec56c
tahlil <bsse0803@iit.du.ac.bd>
2022-11-11 22:57:53 +0600

    Parse query parameter for get user asset api

--- pages/api/blockchain/get_private_asset.js
+++ pages/api/blockchain/get_private_asset.js
@@ -1,12 +1,12 @@
 export default function handler(req, res) {
-    if (req.method === 'GET') {
-      const { asset_id, metamask_id } = req.query;
-        if(asset_id === null || metamask_id === null){
-            res.status(400).json({msg: 'Bad request'});
-        }
-        res.status(200).json({name: "John Doe"})
-    }
-    else{
+  if (req.method === 'GET') {
+    const { asset_id, metamask_id } = req.query;
+    if (asset_id === null || metamask_id === null) {
       res.status(400).json({ msg: 'Bad request' });
     }
+    res.status(200).json({ name: "John Doe" })
+  }
+  else {
+    res.status(400).json({ msg: 'Bad request' });
+  }
 }
