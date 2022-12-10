fc4d848c1544324318cb1db9dd3c52180be7b16c
MunibBukhari <munib.bukhari@graana.com>
2022-12-10 19:37:20 +0500

    update config

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-
-export const aeroAddress = "0x0A03fd09a6906d77cDabC47E46B48d04F1A7A10e"
-export const nftmarketaddress = "0xF99B3BcF328A4Ac305d96F7cBA95159F180FbfB6"
-export const nftaddress = "0x371b3a6e93a625b6304293F17Cb79a912B3F5395"
-xport const nftmarketaddressV2 = "0xfF1E2DD362Da94955713eD23A419c43624e0813a"
-export const nftaddressV2 = "0xB58d50E712c455E34f91F97A168Bfa1c3c08706c"
+export const aeroAddress = '0x0A03fd09a6906d77cDabC47E46B48d04F1A7A10e'
+export const nftmarketaddress = '0xF99B3BcF328A4Ac305d96F7cBA95159F180FbfB6'
+export const nftaddress = '0x371b3a6e93a625b6304293F17Cb79a912B3F5395'
+export const nftmarketaddressV2 = '0xfF1E2DD362Da94955713eD23A419c43624e0813a'
+export const nftaddressV2 = '0xB58d50E712c455E34f91F97A168Bfa1c3c08706c'
