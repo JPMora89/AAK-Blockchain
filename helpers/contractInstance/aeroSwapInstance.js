529c2f32f12d57ffeb47bede4393e72878974a4c
Gauthami75 <gauthamiv75@gmail.com>
2023-06-15 19:50:33 +0530

    buy_aero_token updated

--- helpers/contractInstance/aeroSwapInstance.js
+++ helpers/contractInstance/aeroSwapInstance.js
@@ -1,68 +1,58 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 //import Web3Modal from 'web3modal';
-import path from 'path';
-import fs from 'fs';
-import {aeroSwapAddress, aeroAddress} from '../../config';
-require("dotenv").config({ path: path.resolve(__dirname, '.env.local') })
+import path from "path";
+import fs from "fs";
+import { aeroSwapAddress, aeroAddress } from "../../config";
+require("dotenv").config({ path: path.resolve(__dirname, ".env.local") });
 
-export const infuraId = `https://goerli.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`
+const alchemyId = process.env.ALCHEMY_KEY;
+export const infuraId = `https://eth-sepolia.g.alchemy.com/v2/${alchemyId}`;
 
-export const provider = new ethers.providers.StaticJsonRpcProvider(infuraId)
+export const provider = new ethers.providers.StaticJsonRpcProvider(infuraId);
 //const signer = new ethers.Wallet(process.env.NEXT_PRIVATE_KEY, provider)
 
-
 const aeroSwapContractAbi = () => {
-    try {
-      const dir = path.resolve(
-        './',
-        './artifacts/contracts/Aero-swap.sol/AeroSwap.json'
-      )
-      const file = fs.readFileSync(dir, 'utf8')
-      const json = JSON.parse(file)
-      const abi = json.abi
-      return abi
-    } catch (e) {
-      console.log(`e`, e)
-    }
-  }
-
-  const aeroContractAbi = () => {
-    try {
-      const dir = path.resolve(
-        './',
-        './artifacts/contracts/v2/Aero.sol/Aero.json'
-      )
-      const file = fs.readFileSync(dir, 'utf8')
-      const json = JSON.parse(file)
-      const abi = json.abi
-      return abi
-    } catch (e) {
-      console.log(`e`, e)
-    }
-  }
-
-
-  // export const createAeroSwapInstance = new ethers.Contract(
-  //   aeroSwapAddress,
-  //   aeroSwapContractAbi(),
-  //   signer
-  // )
-
-  export const createAeroSwapInstance = async(userSigner) => {
-    const signer = new ethers.Wallet(userSigner, provider)
-    return new ethers.Contract(
-      aeroSwapAddress,
-      aeroSwapContractAbi(),
-      signer
+  try {
+    const dir = path.resolve(
+      "./",
+      "./artifacts/contracts/Aero-swap.sol/AeroSwap.json"
     );
+    const file = fs.readFileSync(dir, "utf8");
+    const json = JSON.parse(file);
+    const abi = json.abi;
+    return abi;
+  } catch (e) {
+    console.log(`e`, e);
   }
+};
 
-  export const createAeroInstance = (userSigner) => {
-    const signer = new ethers.Wallet(userSigner , provider)
-    return new ethers.Contract(
-      aeroAddress,
-      aeroContractAbi(),
-      signer
+const aeroContractAbi = () => {
+  try {
+    const dir = path.resolve(
+      "./",
+      "./artifacts/contracts/v2/Aero.sol/Aero.json"
     );
+    const file = fs.readFileSync(dir, "utf8");
+    const json = JSON.parse(file);
+    const abi = json.abi;
+    return abi;
+  } catch (e) {
+    console.log(`e`, e);
   }
-  
+};
+
+// export const createAeroSwapInstance = new ethers.Contract(
+//   aeroSwapAddress,
+//   aeroSwapContractAbi(),
+//   signer
+// )
+
+export const createAeroSwapInstance = async (signer) => {
+  //const signer = new ethers.Wallet(userSigner, provider);
+  return new ethers.Contract(aeroSwapAddress, aeroSwapContractAbi(), signer);
+};
+
+export const createAeroInstance = (userSigner) => {
+  const signer = new ethers.Wallet(userSigner, provider);
+  return new ethers.Contract(aeroAddress, aeroContractAbi(), signer);
+};
--- pages/api/blockchain/buy_aero_token.js
+++ pages/api/blockchain/buy_aero_token.js
@@ -4,41 +4,29 @@ import {
   provider,
 } from "../../../helpers/contractInstance/aeroSwapInstance";
 import { aeroSwapAddress } from "../../../config";
-//import cors from "cors";
 
+var signer;
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    //Check if data is present
-    const numberOfTokens = req.body.numberOfTokens;
-    const userAddress = req.body.userAddress;
-    console.log("numberOfTokens", numberOfTokens);
-    console.log("userAddress", userAddress);
-    if (
-      numberOfTokens === undefined ||
-      numberOfTokens === "" ||
-      numberOfTokens === 0
-    ) {
-      res
-        .status(400)
-        .json({ success: false, msg: "Number of tokens is required." });
-      return;
-    }
-
-    const feePercent = 3;
+    const { numberOfTokens, signature, address } = req.body;
+    
+    //verifying signature
+  signer = await verifySignature(numberOfTokens, signature, address);
+  if(signer){
+    const aeroSwapInstance = await createAeroSwapInstance(signer);
+    //Checking if data is present
+    const feePercent = 3.65;
     const tokenPrice = 0.0168;
 
     //Calculating total fee amount to pay
     const numberOfTokensinNum = Number(numberOfTokens);
     const numberOftokensinWei = ethers.utils.parseEther(String(numberOfTokens));
-    const totalfee = (numberOfTokensinNum * feePercent) / 100;
-    const totalAmount = (numberOfTokensinNum - totalfee) * tokenPrice;
+    const pricePerToken = tokenPrice + tokenPrice * (feePercent / 100);
+    const totalAmount = numberOfTokensinNum * pricePerToken;
     const totalAmountinWei = ethers.utils.parseEther(String(totalAmount));
-    const signer = provider.getSigner(userAddress);
-
-    //Set up contract instance
-    const aeroSwapInstance = createAeroSwapInstance(aeroSwapAddress, signer);
-    console.log(aeroSwapInstance);
-    //Buy tokens
+  //   //Set up contract instance
+    //const aeroSwapInstance = await createAeroSwapInstance(signer);
+   // Buy tokens
     try {
       const tx = await aeroSwapInstance.functions.buyTokens(
         numberOftokensinWei,
@@ -56,7 +44,25 @@ export default async function handler(req, res) {
         msg: "Transaction Failed. Please try again.",
       });
     }
+  }else{
+    res.status(400).json({ success: false, msg: "Invalid Signer" });
+  }
   } else {
     res.status(400).json({ success: false, msg: "Bad request." });
+    }
+}
+
+//Verifying the signature
+async function verifySignature(numberOfTokens, signature, address) {
+  try {
+    const signingAddress = await ethers.utils.verifyMessage(numberOfTokens, signature);
+    signingAddress.toLowerCase() === address.toLowerCase();
+    const signer = await provider.getSigner(signingAddress);
+    //const provide = provider.getSigner(signingAddress);
+    return signer;
+
+  } catch (error) {
+    console.error("error3",error);
+    return false;
   }
 }
